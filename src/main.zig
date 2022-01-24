c12eb93cade61f459d54adb3213f51d92a31e046
Lucas Ransan <lucas@ransan.tk>
2022-01-24 16:23:49 +0100

    don't alias

--- src/main.zig
+++ src/main.zig
@@ -1,51 +1,45 @@
 const std = @import("std");
-const log = std.log;
-const fs = std.fs;
-const os = std.os;
-const linux = os.linux;
-const assert = std.debug.assert;
-const CLONE = linux.CLONE;
 const sys = @import("sys.zig");
 
 pub fn main() !void {
-    const inpipe = try os.pipe();
-    const outpipe = try os.pipe();
+    const inpipe = try std.os.pipe();
+    const outpipe = try std.os.pipe();
 
     const tmpdir_path = "/tmp/container";
-    try os.mkdir(tmpdir_path, 0o700);
-    var tmpdir = try fs.openDirAbsolute(tmpdir_path, .{});
+    try std.os.mkdir(tmpdir_path, 0o700);
+    var tmpdir = try std.fs.openDirAbsolute(tmpdir_path, .{});
 
-    log.info("calling {s}", .{os.argv[1..os.argv.len]});
+    std.log.info("calling {s}", .{std.os.argv[1..std.os.argv.len]});
     const child_pid = try sys.clone3(.{
-        .flags = CLONE.NEWUSER | CLONE.NEWNS | CLONE.NEWNET | CLONE.NEWPID,
+        .flags = std.os.linux.CLONE.NEWUSER | std.os.linux.CLONE.NEWNS | std.os.linux.CLONE.NEWNET | std.os.linux.CLONE.NEWPID,
     });
     if (child_pid == 0) {
-        os.close(inpipe[1]);
-        os.close(outpipe[0]);
-        try os.dup2(inpipe[0], 0);
-        try os.dup2(outpipe[1], 1);
-        os.close(inpipe[0]);
-        os.close(outpipe[1]);
+        std.os.close(inpipe[1]);
+        std.os.close(outpipe[0]);
+        try std.os.dup2(inpipe[0], 0);
+        try std.os.dup2(outpipe[1], 1);
+        std.os.close(inpipe[0]);
+        std.os.close(outpipe[1]);
 
         try tmpdir.setAsCwd();
         tmpdir.close();
         try sys.chroot(".");
 
-        return os.execvpeZ(os.argv[1], @ptrCast([*:null]?[*:0]u8, &os.argv[1]), @ptrCast([*:null]?[*:0]u8, os.environ.ptr));
+        return std.os.execvpeZ(std.os.argv[1], @ptrCast([*:null]?[*:0]u8, &std.os.argv[1]), @ptrCast([*:null]?[*:0]u8, std.os.environ.ptr));
     }
 
-    os.close(inpipe[0]);
-    os.close(outpipe[1]);
+    std.os.close(inpipe[0]);
+    std.os.close(outpipe[1]);
 
-    os.close(inpipe[1]);
+    std.os.close(inpipe[1]);
 
-    while ((try sys.splice(outpipe[0], null, 1, null, std.math.maxInt(u32), os.STDOUT_FILENO)) != 0) {}
-    os.close(outpipe[0]);
+    while ((try sys.splice(outpipe[0], null, 1, null, std.math.maxInt(u32), std.os.STDOUT_FILENO)) != 0) {}
+    std.os.close(outpipe[0]);
 
     const rc = try sys.wait4(child_pid, 0);
-    assert(rc.pid == child_pid);
-    assert(linux.W.IFEXITED(rc.status) and linux.W.EXITSTATUS(rc.status) == 0);
-    log.info("{}", .{rc});
+    std.debug.assert(rc.pid == child_pid);
+    std.debug.assert(std.os.linux.W.IFEXITED(rc.status) and std.os.linux.W.EXITSTATUS(rc.status) == 0);
+    std.log.info("{}", .{rc});
 
     try tmpdir.deleteTree(".");
     tmpdir.close();
